8473d0ec67f61052169770735ae554a8f7c16870
ZRune07 <yilongxingyu@outlook.com>
2020-09-09 11:32:51 +0800

    test1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Switch, Route, HashRouter,BrowserRouter} from 'react-router-dom'
+import './App.scss';
+import Home from './views/Home';
+import About from './views/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = {};
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>test</h1>
+        <BrowserRouter>
+          <Switch>
+            <Route component={Home} exact path="/"/>
+            <Route component={About} path="/about" />
+          </Switch>
+          {/* HashRouter用URL中的hash部分创建路由，URL中会附带# 不需要处理
+          BrowserRouter的URL指向真实的URL地址，上线后需要后台处理URL指向
+          switch路径相同的情况下只匹配第一个
+          exact精准匹配 */}
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/views/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./aaa.scss";
+class Home extends React.Component {
+    constructor(){
+        super();
+        this.state = {};
+    }
+
+    render(){
+        return (
+            <div>
+                <div>Home</div>
+            </div>
+        )
+    }
+}
+
+export default Home;
