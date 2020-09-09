293d3a6be833ee580cd17bde9bd8d7efedb2bc54
ZRune07 <yilongxingyu@outlook.com>
2020-09-10 00:34:59 +0800

    no change

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { Switch, Route, HashRouter,BrowserRouter} from 'react-router-dom'
 import './App.scss';
-import Home from './views/Home';
-import About from './views/About';
+import Login from './views/login/index';
 
 class App extends React.Component{
   constructor(){
@@ -12,19 +11,15 @@ class App extends React.Component{
 
   render(){
     return(
-      <div>
-        <h1>test</h1>
         <BrowserRouter>
           <Switch>
-            <Route component={Home} exact path="/"/>
-            <Route component={About} path="/about" />
+            <Route component={Login} exact path="/"/>
           </Switch>
           {/* HashRouter用URL中的hash部分创建路由，URL中会附带# 不需要处理
           BrowserRouter的URL指向真实的URL地址，上线后需要后台处理URL指向
           switch路径相同的情况下只匹配第一个
           exact精准匹配 */}
         </BrowserRouter>
-      </div>
     );
   }
 }
--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,8 +1,31 @@
 import React, {Component} from "react";
-import { PageHeader, Form, Input, Button, Checkbox,Layout, Menu, Breadcrumb } from 'antd';
+import { PageHeader, Form, Input, Button, Checkbox,Layout, Menu, Breadcrumb,Space, Card } from 'antd';
 import "./index.scss";
 
 const { Header, Content, Footer } = Layout;
+
+const layout = {
+    labelCol: {
+      span: 8,
+    },
+    wrapperCol: {
+      span: 16,
+    },
+};
+const tailLayout = {
+    wrapperCol: {
+    offset: 8,
+    span: 16,
+    },
+};
+  
+    const onFinish = values => {
+      console.log('Success:', values);
+    };
+
+    const onFinishFailed = errorInfo => {
+      console.log('Failed:', errorInfo);
+    };
 
 class Login extends Component{
     constructor(){
@@ -13,50 +36,63 @@ class Login extends Component{
 
     render(){
         return (
-                <Layout>
+                <Space class="space1">
                     <PageHeader
                         className="site-page-header"
-                        onBack={() => null}
-                        title="Title"
-                        subTitle="This is a subtitle"
+                        title="登录"
+                        subTitle="请输入账号密码"
                     />
-
-                        <form class="form1">
-                            <Form
+                    <div className = "form-warp">
+                            <Card 
+                                hoverable
+                                style={{}}
+                                className = "cardbasic"
+                                {...layout}
                                 name="basic"
                                 initialValues={{
                                     remember: true,
                                 }}
-                            ></Form>
-                            
-                            <Form.Item
-                                label="Username"
-                                name="username"
-                                rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your username!',
-                                },
-                                ]}
-                            >
-                                <Input />
-                            </Form.Item>
+                                onFinish={onFinish}
+                                onFinishFailed={onFinishFailed}
+                                >
+                                <Form.Item
+                                    label="用户名"
+                                    name="username"
+                                    rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your username!',
+                                    },
+                                    ]}
+                                >
+                                    <Input />
+                                </Form.Item>
 
-                            <Form.Item
-                                label="Password"
-                                name="password"
-                                rules={[
-                                {
-                                    required: true,
-                                    message: 'Please input your password!',
-                                },
-                                ]}
-                            >
-                                <Input.Password />
-                            </Form.Item>
-                            
-                        </form>
-                </Layout>
+                                <Form.Item
+                                    label="密码"
+                                    name="password"
+                                    rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your password!',
+                                    },
+                                    ]}
+                                >
+                                    <Input.Password />
+                                </Form.Item>
+
+                                <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+                                    <Checkbox>Remember me</Checkbox>
+                                </Form.Item>
+
+                                <Form.Item {...tailLayout}>
+                                    <Button type="primary" htmlType="submit">
+                                    登录
+                                    </Button>
+                                </Form.Item>
+                                </Card>
+                            </div>
+                </Space>
         )
     }
 }
